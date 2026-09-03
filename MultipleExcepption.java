d26f128c04eb10e487cc2ec38e704010a9fd9769
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-03 19:26:03 +0500

    Update MultipleExcepption.java

--- MultipleExcepption.java
+++ MultipleExcepption.java
@@ -1,21 +1,32 @@
-
-
 import java.util.Scanner;
+
 public class MultipleExcepption {
+    
     public static void main(String[]args){
+        
         Scanner sc=new Scanner(System.in);
+        
     int arr[]=new int[5];
   
     try{
+        
          arr[6]=10;
+        
     }
+        
     catch(ArithmeticException e){
+        
     System.out.println("Divisor cant be zero");
     
 }
+        
     catch(ArrayIndexOutOfBoundsException e){
+        
     System.out.println("e.getMessage()");
+        
     }
-    }
+        
+ }
+    
 }
 
