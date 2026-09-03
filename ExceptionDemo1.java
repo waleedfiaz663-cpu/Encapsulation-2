a820d759c8bc3697ce58be6b2f2855f47af08011
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-03 19:24:29 +0500

    Update ExceptionDemo1.java

--- ExceptionDemo1.java
+++ ExceptionDemo1.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class ExceptionDemo1 {
+    
     public static void main(String[]args){
+        
         Scanner Sc = new Scanner(System.in);
+        
         int divident=Sc.nextInt();
+        
         int divisor=Sc.nextInt();
+        
         try{
-        int result=divident/divisor;
-        System.out.println("Result is : "+result);
-    }
-        catch(ArithmeticException e){
-            System.out.println("Divisor cant be zero");
-        }
             
-        }
+        int result=divident/divisor;
+            
+        System.out.println("Result is : "+result);
+            
+    }
+            
+        catch(ArithmeticException e){
+            
+            System.out.println("Divisor cant be zero");
+            
+    }
+            
+ }
+    
 }
